0dd5c2d9efc441f221b28f2765c8d38c507cc5bf
Codingwithriha <rihashehzadi2003@gmail.com>
2026-04-13 10:01:23 +0500

    Add migrations for students and student_profiles tables, and create StudentFactory

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Student CRUD Routes (all 7 methods) 
+Route::resource('students', StudentController::class); 
+ 
+// OR you can write individually: 
+// Route::get('/students', [StudentController::class, 'index'])->name('students.index'); 
+// Route::get('/students/create', [StudentController::class, 'create'])->name('students.create'); 
+// Route::post('/students', [StudentController::class, 'store'])->name('students.store'); 
+// Route::get('/students/{student}', [StudentController::class, 'show'])->name('students.show'); 
+// Route::get('/students/{student}/edit', [StudentController::class, 'edit'])->name('students.edit'); 
+// Route::put('/students/{student}', [StudentController::class, 'update'])->name('students.update'); 
+// Route::delete('/students/{student}', [StudentController::class, 'destroy'])->name('students.destroy'); 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/students', function () { 
+//     return view('students.index'); 
+// }); 
+?>
