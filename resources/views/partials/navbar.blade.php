a191c52dfe7e44bbb7b2170d03fef997d8018a9c
Codingwithriha <rihashehzadi2003@gmail.com>
2026-04-13 10:01:53 +0500

    Add footer, navbar, and sidebar partials for layout structure

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,12 @@
+<div class="navbar"> 
+    <div> 
+        <strong>         School System</strong> 
+        <a href="/">Home</a> 
+        <a href="#">About</a> 
+        <a href="#">Contact</a> 
+    </div> 
+ 
+    <div> 
+        <a href="#">Logout</a> 
+    </div> 
+</div> 
---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,7 @@
+<div class="sidebar"> 
+ 
+    <a href="/students">                           Students</a> 
+    <a href="#">                                       Teachers</a> 
+    <a href="#">             Courses</a> 
+ 
+</div>
